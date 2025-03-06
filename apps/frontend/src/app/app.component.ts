42d74cd53a397a57b1932157b249773070522322
Kasper Nielsen <kasperni@gmail.com>
2025-03-06 08:28:52 +0100

    Adding primeng

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from "../test/button.component";
 
 @Component({
-  imports: [ RouterModule],
+  imports: [RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
