e8f2ed55e74249cd6d84e3ba35888fa3fc424523
Kasper Nielsen <kasperni@gmail.com>
2025-07-06 12:50:55 +0200

    updates

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello API' };
+    return { message: 'Hello API!' };
   }
 }
 // Updated Tir 10 Jun 2025 12:42:01 CEST
