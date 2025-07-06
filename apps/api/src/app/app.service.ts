ca369f919bea05a4e21872ca0da57a7b320970f4
Kasper Nielsen <kasperni@gmail.com>
2025-07-06 13:00:52 +0200

    chore: Clean up app.service.ts to trigger deployment
    Remove unnecessary comments to ensure latest code is deployed to dev environment

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-//
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API!' };
   }
 }
-// Updated Tir 10 Jun 2025 12:42:01 CEST
